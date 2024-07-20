0fcaa60ad4635764ebc742aba3be8858a0ee59c3
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-20 12:48:06 -0400

    adding functions for %i and %d

--- printf.c
+++ printf.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_number - Helper function to print an integer
+ * @num: The integer to print
+ */
+void print_number(int num)
+{
+    if (num < 0)
+    {
+        _putchar('-');
+        num = -num;
+    }
+    if (num / 10)
+        print_number(num / 10);
+    _putchar((num % 10) + '0');
+}
+
+/**
  * printf - prints out text from a unknown number of arguments
  *
  * @format: format specifier
@@ -10,26 +26,79 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i, count = 0;
+	int num;
+	int i = 0;
+	int count = 0;
 
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format)
 	{
-
-	if (format[i] != '%')
+		if (*format != '%')
 		{
-		_putchar (format[i]);
+			write(1, format, 1);
+			count++;
 		}
-	if (format[i] == '%' && format[i + 1]) == 'c'
-	{
-		_putchar(va_arg(args, int))
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				count++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+            {
+                num = va_arg(args, int);
+                if (num < 0)
+                {
+                    _putchar('-');
+                    count++;
+                    num = -num;
+                }
+                print_number(num);
+                {
+                    int temp = num;
+                    while (temp)
+                    {
+                        count++;
+                        temp /= 10;
+                    }
+                }
+            }
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+					str_len++;
+
+				write(1, str, str_len);
+				count += str_len;
+			}
 		}
-	count += 1;
+
+		format++;
 	}
-}
+
 	va_end(args);
+
 	return (count);
 
+}
