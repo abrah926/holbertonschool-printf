d551e20743ff6debbe1504450804eb3b2c8e97c7
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-07-29 09:03:09 -0400

    update it to include struct logic

--- main.h
+++ main.h
@@ -1,13 +1,24 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+#include <limits.h>
 
-int _printf(const char *format, ...);
-void print_number(int num);
+/* Function prototypes */
 int _putchar(char c);
+int _printf(const char *format, ...);
+int (*op_print(char s))(va_list);
+int op_char(va_list args);
+int op_str(va_list args);
+int op_int(va_list args);
+int op_percent(va_list args);
+void print_number(int num);
+
+typedef struct specifier
+{
+    char spec;
+    int (*func)(va_list);
+} specifier_t;
 
 #endif
